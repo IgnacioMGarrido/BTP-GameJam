5124e64deaecb2ec8783c3aa45238d7c63e89459
Ignacio <nama1510@gmail.com>
2019-02-24 17:25:29 +0100

    Added Gravity to bullets

--- BTP-Game/Scripts/Bullet.gd
+++ BTP-Game/Scripts/Bullet.gd
@@ -2,20 +2,39 @@ extends KinematicBody2D
 
 class_name Bullet
 
-var speed : float = 75
+const G = 200000
+var center : Vector2
+
+var speed : float = 60
 var velocity : Vector2
+
+func _ready():
+	center = get_node("/root/MainScene/Planet").global_position
 
 func start(pos, dir):
 	rotation = dir
 	position = pos
 	velocity = Vector2(speed, 0).rotated(rotation)
 
-func _physics_process(delta):
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		velocity = velocity.bounce(collision.normal)
-		if collision.collider.has_method("hit"):
-			collision.collider.hit()
+func acceleration(pos1 : Vector2, pos2 : Vector2):
+	var direction = pos1 - pos2
+	var length = sqrt(pow(direction.x, 2) + pow(direction.y, 2))
+	var normal_dir = direction.normalized()
+	
+	return Vector2(normal_dir.x * (G / pow(length,2)), normal_dir.y * (G / pow(length,2)))
 
+
+
+func _physics_process(delta):
+	velocity += acceleration(center, self.position) * delta
+	var collision = move_and_collide(velocity * delta)
+	
+	
+#	if collision:
+#		velocity = velocity.bounce(collision.normal)
+#		if collision.collider.has_method("hit"):
+#			collision.collider.hit()
+
+	
 func _on_VisibilityNotifier2D_screen_exited():
     queue_free()
