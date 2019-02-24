31e6c4d991aca9f321b19995680cb14f2d923b53
Alejandro de Cea <alejandrodecea@gmail.com>
2019-02-24 05:04:23 +0100

    Revamped turret system, added MainPlanet and DefenseStrap sprites

--- BTP-Game/Scripts/DefenseSpots.gd
+++ BTP-Game/Scripts/DefenseSpots.gd
@@ -1,8 +1,7 @@
-extends KinematicBody2D
+extends Node2D
 
 var rot_speed = .2 #deg/second
 
 func _physics_process(delta):
 	# rotate the planet
 	rotate(rot_speed * delta)
-	
--- BTP-Game/Scripts/Turret.gd
+++ BTP-Game/Scripts/Turret.gd
@@ -4,16 +4,15 @@ class_name Turret
 
 var is_active = true
 onready var pivot : Position2D = $TurretPivot
-onready var bullet_spawn : Position2D = $TurretPivot/Sprite/ProjectilSpawn
+onready var bullet_spawn : Position2D = $TurretPivot/Sprite/ProjectileSpawn
 var Bullet = load(Global.bullet_res)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	update_motion(delta)
-	
+
 
 func update_motion(delta):
-	
 	if is_active:
 		pivot.look_at(get_global_mouse_position())
 		if Input.is_action_just_pressed("ui_accept"):
@@ -23,6 +22,4 @@ func update_motion(delta):
 func shoot_projectile():
 	var b = Bullet.instance()
 	b.start(bullet_spawn.global_position, bullet_spawn.global_rotation)
-	get_parent().add_child(b)
-
-	
+	get_node("/root/MainScene").add_child(b)
