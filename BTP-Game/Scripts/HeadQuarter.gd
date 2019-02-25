65ab2882a582471f626de7923e39e1d6ed5c6346
Ignacio <nama1510@gmail.com>
2019-02-25 19:56:36 +0100

    Workers Fixed

--- BTP-Game/Scripts/HeadQuarter.gd
+++ BTP-Game/Scripts/HeadQuarter.gd
@@ -2,15 +2,11 @@ extends Area2D
 
 var Minion = load(Global.minion_res)
 
-func _ready():
-#	var m = Minion.instance()
-#
-#	add_child(m)
-	pass
 
 func _on_Timer_timeout():
-	#var m = Minion.instance()
-#	add_child(m)
-#	get_node("..").add_child(m)
-	pass
+	if get_child_count() < 6:
+		var m = Minion.instance()
+		add_child(m)
+
+
 	
--- BTP-Game/Scripts/minion.gd
+++ BTP-Game/Scripts/minion.gd
@@ -4,8 +4,8 @@ class_name Minion
 
 signal mineral_delivered 
 
-onready var head_quarter = get_node("/root/MainScene/Planet/ResourceManagement/HeadQuarter").global_position 
-onready var mine = get_node("/root/MainScene/Planet/ResourceManagement/Mine").global_position
+onready var head_quarter = get_parent()#get_node("/root/MainScene/Planet/ResourceManagement/HeadQuarter") 
+onready var mine = get_parent().get_parent().get_child(1)#get_node("/root/MainScene/Planet/ResourceManagement/Mine")
 onready var mineral_sprite = $Sprite/mineral_sprite
 
 
@@ -16,8 +16,10 @@ var speed = 10
 
 var mineral_value = 10
 func _ready():
+	head_quarter.connect("body_shape_entered", self, "_on_HeadQuarter_body_shape_entered")
+	mine.connect("body_shape_entered", self, "_on_Mine_body_shape_entered")
 	mineral_sprite.visible = false
-	destination = mine
+	destination = mine.global_position
 
 func _physics_process(delta):
 	direction = (destination - global_position).normalized()
@@ -27,15 +29,12 @@ func _physics_process(delta):
 
 func _on_Mine_body_shape_entered(body_id, body, body_shape, area_shape):
 	if body == self:
-		print("Mine")
-		destination = head_quarter
-		print(destination)
+		destination = head_quarter.global_position
 		mineral_sprite.visible = true
 
 
 func _on_HeadQuarter_body_shape_entered(body_id, body, body_shape, area_shape):
 	if body == self:
-		destination = mine
-		print(destination)
+		destination = mine.global_position
 		emit_signal("mineral_delivered",mineral_value)
 		mineral_sprite.visible = false
