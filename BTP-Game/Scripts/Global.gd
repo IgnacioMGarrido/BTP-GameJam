216943a172accd071561e7e10b09af42a93791da
Ignacio <nama1510@gmail.com>
2019-02-26 17:29:54 +0100

    Turrets now cost minerals

--- BTP-Game/Scripts/Global.gd
+++ BTP-Game/Scripts/Global.gd
@@ -1,6 +1,10 @@
 extends Node
 
+#File System Paths
 const bullet_res = "res://Scenes/Planets/Defenses/Bullet.tscn"
 const planet_main = "res://Scenes/MainScene.tscn"
 const minion_res = "res://Scenes/minions/minion.tscn"
 const turret_res = "res://Scenes/Planets/Defenses/Turret.tscn"
+
+#Nodes
+var ResourceManager_path = "/root/MainScene/Planet/ResourceManagement"
