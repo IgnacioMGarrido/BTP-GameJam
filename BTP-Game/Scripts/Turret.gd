9688e4312f0e394f5a07356eba523a0d6c29a1fd
Alejandro de Cea <alejandrodecea@gmail.com>
2019-02-26 19:54:50 +0100

    Mirrored rotation in the turret when following cursor

--- BTP-Game/Scripts/Turret.gd
+++ BTP-Game/Scripts/Turret.gd
@@ -2,18 +2,34 @@ extends StaticBody2D
 
 class_name Turret
 
-
+var PI_2 = PI/2
 var turret_cost = 100
 
 var active = true
 onready var pivot : Position2D = $TurretPivot
+onready var dummy_pivot : Position2D = $DummyPivot
 onready var bullet_spawn : Position2D = $TurretPivot/Sprite/ProjectileSpawn
 var Bullet = load(Global.bullet_res)
 
 
 func update_motion(delta):
 	if active:
-		pivot.look_at(get_global_mouse_position())
+		dummy_pivot.look_at(get_global_mouse_position())
+		
+		#Reset rotation value to 0 if it's beyond 180 or -180 degrees
+		if dummy_pivot.rotation > PI:
+			dummy_pivot.rotation -= PI
+		elif dummy_pivot.rotation < -PI:
+			dummy_pivot.rotation += PI
+		
+		#Mirror rotation beyond 90 or -90 degrees
+		if dummy_pivot.rotation > PI_2:
+			pivot.rotation = dummy_pivot.rotation - 2*(dummy_pivot.rotation - PI_2)
+		elif dummy_pivot.rotation < -PI_2:
+			pivot.rotation = dummy_pivot.rotation - 2*(dummy_pivot.rotation + PI_2)
+		else:
+			pivot.rotation = dummy_pivot.rotation
+		
 		if Input.is_action_just_pressed("ui_accept"):
 			shoot_projectile()
 
