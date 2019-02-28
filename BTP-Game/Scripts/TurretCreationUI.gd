fe9b2f0464e7e5cb713e91d08decf5869a9db339
Ignacio <nama1510@gmail.com>
2019-03-01 00:51:17 +0100

    Minor fix: TurretCreation UI bug fixed

--- BTP-Game/Scripts/TurretCreationUI.gd
+++ BTP-Game/Scripts/TurretCreationUI.gd
@@ -5,10 +5,10 @@ signal laserTurretSelected
 
 
 func _on_BasicTurretButton_pressed():
-	emit_signal("basicTurretSelected",get_tree().paused)
+	emit_signal("basicTurretSelected")
 	
 
 
 func _on_LaserTurretButton_pressed():
-	emit_signal("laserTurretSelected", get_tree().paused)
+	emit_signal("laserTurretSelected")
 		
