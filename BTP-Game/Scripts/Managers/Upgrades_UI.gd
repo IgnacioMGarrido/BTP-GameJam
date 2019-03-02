547bfe5ce482207a84f15f61fd29c04b08854189
Ignacio <nama1510@gmail.com>
2019-03-02 14:18:51 +0100

    Added spawn turret sfx and upgrade sfx

--- BTP-Game/Scripts/Managers/Upgrades_UI.gd
+++ BTP-Game/Scripts/Managers/Upgrades_UI.gd
@@ -11,6 +11,7 @@ onready var res_manager = get_node(Global.ResourceManager_path)
 
 func _on_BulletUpgradeButton_pressed():
 	if res_manager.current_minerals >= basic_upgrade_cost:
+		$"AudioStreamPlayer".play()
 		res_manager._substract_minerals(basic_upgrade_cost)
 		bulletButton.disabled = true
 		visible = false
